982024547a6874744e7dc5d6eb4d9fa24a179dab
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-05-28 23:55:28 +0100

    showMessage and ui.create element

--- scripts/AttendMe/levelup_indicator.lua
+++ scripts/AttendMe/levelup_indicator.lua
@@ -1,46 +1,38 @@
 local ui = require('openmw.ui')
-local I = require('openmw.interfaces')
-local storage = require('openmw.storage')
+local util = require('openmw.util')
+-- local calendar = require('openmw_aux.calendar')
+local time = require('openmw_aux.time')
 
-I.Settings.registerGroup({
-    key = 'SettingsAttendMeUI',
-    page = 'AttendMe',
-    l10n = 'AttendMe_UISettings',
-    name = "group_name",
-    permanentStorage = false,
-    settings = {
-        {
-            key = 'showLevelUpIndicator',
-            name = "showLevelUpIndicator_name",
-            default = true,
-            renderer = 'checkbox',
-        },
-    },
-})
-
-local uiSettings = storage.globalSection('SettingsAttendMeUI')
-
-return {
-    engineHandlers = {
-        -- onLevelUp = function()
-        --     if uiSettings:get('showLevelUpIndicator') then
-        --         ui.showMessageBox({
-        --             message = "You have leveled up!",
-        --             buttons = { "OK" },
-        --         })
-        --     end
-        -- end,
-        -- onKeyPress = function(key)
-        --     if key.symbol == 'x' then
-        --         ui.showMessage('You have pressed "X"')
-        --     end
-        -- end
-        onLoad = function()
-            ui.showMessage('Hello, Morrowind!')
-            -- ui.showMessageBox({
-            --     message = "Welcome to AttendMe!",
-            --     buttons = { "OK" },
-            -- })
-        end,
+local element = ui.create {
+    -- important not to forget the layer
+    -- by default widgets are not attached to any layer and are not visible
+    layer = 'HUD',
+    type = ui.TYPE.Text,
+    props = {
+        -- position in the top right corner
+        relativePosition = util.vector2(1, 0),
+        -- position is for the top left corner of the widget by default
+        -- change it to align exactly to the top right corner of the screen
+        anchor = util.vector2(1, 0),
+        -- text = calendar.formatGameTime('%H:%M'),
+        text = "LEVEL UP!",
+        textSize = 24,
+        -- default black text color isn't always visible, lime green is better
+        textColor = util.color.rgb(0, 1, 0),
     },
 }
+
+local function updateTime()
+    -- ui.log('Updating time')
+    ui.showMessage("LEVEL UP!")
+    -- formatGameTime uses current time by default
+    -- otherwise we could get it by calling `core.getGameTime()`
+    -- element.layout.props.text = calendar.formatGameTime('%H:%M')
+    element.layout.props.text = "LEVEL UP!"
+    -- the layout changes won't affect the widget unless we request an update
+    element:update()
+end
+
+-- we are showing game time in hours and minutes
+-- so no need to update more often than once a game minute
+time.runRepeatedly(updateTime, 1 * time.minute, { type = time.GameTime })
