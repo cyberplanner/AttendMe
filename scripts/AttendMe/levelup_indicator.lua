25957781089076a79828d11cda35f2c386e99f24
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-05-29 20:22:51 +0100

    Formatting and colours

--- scripts/AttendMe/levelup_indicator.lua
+++ scripts/AttendMe/levelup_indicator.lua
@@ -10,15 +10,34 @@ local element = ui.create {
     type = ui.TYPE.Text,
     props = {
         -- position in the top right corner
-        relativePosition = util.vector2(1, 0),
+        relativePosition = util.vector2(0.9, 0.05),
         -- position is for the top left corner of the widget by default
         -- change it to align exactly to the top right corner of the screen
         anchor = util.vector2(1, 0),
         -- text = calendar.formatGameTime('%H:%M'),
-        text = "TEST!",
-        textSize = 18,
+        text = "Initiating Level Indicator...",
+        textSize = 14,
         -- default black text color isn't always visible, lime green is better
         textColor = util.color.rgb(158, 0, 237),
+    },
+}
+
+local progressElement = ui.create {
+    -- important not to forget the layer
+    -- by default widgets are not attached to any layer and are not visible
+    layer = 'HUD',
+    type = ui.TYPE.Text,
+    props = {
+        -- position in the top right corner
+        relativePosition = util.vector2(0.9, 0.07),
+        -- position is for the top left corner of the widget by default
+        -- change it to align exactly to the top right corner of the screen
+        anchor = util.vector2(1, 0),
+        -- text = calendar.formatGameTime('%H:%M'),
+        text = "Initiating Level Indicator...",
+        textSize = 12,
+        -- default black text color isn't always visible, lime green is better
+        textColor = util.color.rgb(1, 1, 1),
     },
 }
 
@@ -40,14 +59,18 @@ local function updateTime()
     -- otherwise we could get it by calling `core.getGameTime()`
     -- element.layout.props.text = calendar.formatGameTime('%H:%M')
     -- ui.showMessage("LEVEL UP!" .. self.object.name)
+    progressElement.layout.props.text = ""
     if isReadyToLevelUp then
         ui.showMessage("Ready to level up...")
-        element.layout.props.text = "LEVEL: " .. tostring(stats.current) .. "  * Advance to lvl: " .. tostring(nextLevel)
+        element.layout.props.text = "LEVEL: " .. tostring(stats.current)
+        progressElement.layout.props.text = "Advance to lvl: " .. tostring(nextLevel) .. "!!"
     else
-        element.layout.props.text = "LEVEL: " .. tostring(stats.current) .. "  * Progress: " .. tostring(progress .. " / 10")
+        element.layout.props.text = "LEVEL: " .. tostring(stats.current)
+        progressElement.layout.props.text = "Progress: " .. tostring(progress) .. "/10"
     end
     -- the layout changes won't affect the widget unless we request an update
     element:update()
+    progressElement:update()
 end
 
 -- we are showing game time in hours and minutes
